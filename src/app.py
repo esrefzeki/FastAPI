ba2417aa356970cb696526c7f9ecf5e83421e9fa
Esref Zeki PARLAK <54935247+esrefzeki@users.noreply.github.com>
2022-01-21 15:33:23 +0300

    Basic endpoints added

---
+++ src/app.py
@@ -0,0 +1,59 @@
+from fastapi import FastAPI, Response, status, HTTPException
+from typing import Optional
+from fastapi.params import Body
+from pydantic import BaseModel
+from random import randrange
+
+
+app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[str] = None
+
+my_post = [{"title": "title of the content - 1", "content": "content of the post - 1", "id": 1},
+           {"title": "title of the content - 2", "content": "content of the post - 2", "id": 2}]
+
+def find_post(id):
+    for p in my_post:
+        if p['id'] == id:
+            return p
+
+def find_index_post(id):
+    for i, p in enumerate(my_post):
+        if p['id'] == id:
+            return i
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/posts")
+async def get_posts():
+    return {"data": my_post}
+
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def create_posts(post: Post):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 110000)
+    my_post.append(post_dict)
+    return {"data": post_dict}
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    post = find_post(id)
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    # deleting post
+    # find the index in the array that has required ID
+    # my_posts.pop(index)
+    index = find_index_post(id)
+
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"The post {id} does not exist.")
+    return {"info": f"The post which has id {id} removed."}
