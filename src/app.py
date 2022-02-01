135df40a476de15d2bbcf3fbb21fed2fe41e3b35
Esref Zeki PARLAK <54935247+esrefzeki@users.noreply.github.com>
2022-02-01 19:44:36 +0300

    User and Post Seperated

--- src/app.py
+++ src/app.py
@@ -3,18 +3,21 @@ from fastapi import FastAPI, Response, status, HTTPException, Depends
 from typing import Optional, List
 from fastapi.params import Body
 from pydantic import BaseModel
-from random import randrange
 from psycopg2.extras import RealDictCursor
 from FastAPI.src.db_manager import engine, get_db
 from FastAPI.src.api.models import models
 from FastAPI.src.security import utility
 from sqlalchemy.orm import Session
 from FastAPI.src.api.models.dto import post_crud, users_dto
+from FastAPI.src.routers import post, user
+
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+app.include_router(post.router)
+app.include_router(user.router)
 
 while True:
     try:
@@ -33,114 +36,3 @@ while True:
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-
-@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=post_crud.PostResponse)
-def create_posts(post: post_crud.PostCreate, db: Session = Depends(get_db)):
-    # 1:
-    # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s)
-    # RETURNING * """,
-    #                (post.title, post.content, post.published))
-    # new_post = cursor.fetchone()
-
-    # 2:
-    # new_post = models.Post(title=post.title,
-    #                        content=post.content,
-    #                        published=post.published)
-
-    new_post = models.Post(**post.dict()) # **post.dict() ile istenen düm içerik inputlarını saymış oluyoruz.
-    db.add(new_post)
-    db.commit()
-    db.refresh(new_post)
-
-    return new_post
-
-
-@app.get("/posts/{id}", response_model=post_crud.PostResponse)
-def get_post(id: int, db: Session = Depends(get_db)):
-    the_post = db.query(models.Post).filter(models.Post.id == id).first()
-
-    if not the_post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"The post {id} does not exist.")
-
-    return the_post
-
-
-@app.get("/posts", response_model=List[post_crud.PostResponse])
-def get_posts(db: Session = Depends(get_db)):
-    posts = db.query(models.Post).all()
-    return posts
-
-
-@app.put("/posts/{id}")
-def update_post(id: int, post: post_crud.PostBase, db: Session = Depends(get_db)):
-    post_query = db.query(models.Post).filter(models.Post.id == id)
-    check_it = post_query.first()
-
-    if check_it == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"The post {id} does not exist.")
-
-    post_query.update(post.dict(), synchronize_session=False)
-
-    db.commit()
-
-    return post_query.first()
-
-
-@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db)):
-    # deleting post
-    # find the index in the array that has required ID
-    # my_posts.pop(index)
-    post_delete = db.query(models.Post).filter(models.Post.id == id)
-    deleted = db.query(models.Post).filter(models.Post.id == id).first()
-
-    if deleted == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"The post {id} does not exist.")
-
-    post_delete.delete(synchronize_session=False)
-    db.commit()
-
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
-
-
-@app.post("/user", status_code=status.HTTP_201_CREATED, response_model=users_dto.UserResponse)
-def create_user(user: users_dto.UserCreate, db: Session = Depends(get_db)):
-
-    # hash the password - user.password
-    hashed_password = utility.hash(user.password)
-    user.password = hashed_password
-
-    new_user = models.User(**user.dict())
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-
-    return new_user
-
-
-@app.get("/user/{id}", response_model=users_dto.UserResponse)
-def get_user(id: int, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.id == id).first()
-
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"The user {id} does not exist.")
-
-    return user
-
-
-@app.delete("/user/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_user(id: int, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.id == id)
-    deleted_user = db.query(models.User).filter(models.User.id == id).first()
-
-    if not deleted_user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"The user {id} does not exist.")
-
-    user.delete(synchronize_session=False)
-    db.commit()
-
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
