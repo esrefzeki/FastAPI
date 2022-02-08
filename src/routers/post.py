bca1985f35d39bd3628517d00ff7c6a701a20a2f
Esref Zeki PARLAK <54935247+esrefzeki@users.noreply.github.com>
2022-02-08 14:40:03 +0300

    Search option added

--- src/routers/post.py
+++ src/routers/post.py
@@ -61,11 +61,15 @@ def get_post(id: int, db: Session = Depends(get_db)):
     return the_post
 
 
-@router.get("/", response_model=List[post_crud.PostResponse], )
+@router.get("/", response_model=List[post_crud.PostResponse])
 def get_posts(db: Session = Depends(get_db),
-              current_user: int = Depends(oauth2.get_current_user)):
+              current_user: int = Depends(oauth2.get_current_user),
+              limit: int = 10,
+              skip: int = 0,
+              search: Optional[str] = ""):
 
-    posts = db.query(models.Post).all()
+    posts = db.query(models.Post).filter(models.Post.title.contains(search)).offset(skip).limit(limit).all()
+
     return posts
 
 
