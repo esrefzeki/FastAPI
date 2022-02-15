533ccf3ec225b28775543b0f44b33a4e5cb4d8a6
Esref Zeki PARLAK <54935247+esrefzeki@users.noreply.github.com>
2022-02-15 18:00:46 +0300

    changes

--- src/api/infrastructure/persistance/db_manager.py
+++ src/api/infrastructure/persistance/db_manager.py
@@ -14,6 +14,7 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+some = changes
 
 
 # Dependency:
