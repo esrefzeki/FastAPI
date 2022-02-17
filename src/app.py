b794880acdba526f4d502ada13b2d8b4992bafa8
Esref Zeki PARLAK <54935247+esrefzeki@users.noreply.github.com>
2022-02-17 12:46:56 +0300

    lru

--- src/app.py
+++ src/app.py
@@ -2,11 +2,19 @@ from fastapi import FastAPI
 from FastAPI.src.api.infrastructure.persistance.db_manager import engine
 from FastAPI.src.api.models import models
 from FastAPI.src.routers import post, user, auth
+from FastAPI.src import config
+from functools import lru_cache
 from FastAPI.src.config import GlobalConfig
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+
+@lru_cache()
+def get_settings():
+    return config.GlobalConfig()
+
 
 app.include_router(post.router)
 app.include_router(user.router)
