57843d59e9f9712900d1f5bdb794b4bde40ae8b5
Esref Zeki PARLAK <54935247+esrefzeki@users.noreply.github.com>
2022-03-09 14:16:05 +0300

    alembic model examples

---
+++ src/migrations/versions/2022-03-09_09-26-59_4a332edaaf7c_testing.py
@@ -0,0 +1,24 @@
+"""Testing
+
+Revision ID: 4a332edaaf7c
+Revises: 29bc6f1ac83a
+Create Date: 2022-03-09 09:26:59.831242+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4a332edaaf7c'
+down_revision = '29bc6f1ac83a'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ src/migrations/versions/20220309_092341_29bc6f1ac83a_testing.py
@@ -0,0 +1,24 @@
+"""Testing
+
+Revision ID: 29bc6f1ac83a
+Revises: 
+Create Date: 2022-03-09 09:23:41.952974+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '29bc6f1ac83a'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
