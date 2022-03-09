f8c72b3c024641786b8249358deb8d7c89ca439b
Esref Zeki PARLAK <54935247+esrefzeki@users.noreply.github.com>
2022-03-09 17:13:25 +0300

    alembic worked codes

--- src/migrations/versions/2022-03-09_09-26-59_4a332edaaf7c_testing.py
+++ src/migrations/versions/2022-03-09_09-26-59_4a332edaaf7c_testing.py
@@ -8,7 +8,6 @@ Create Date: 2022-03-09 09:26:59.831242+00:00
 from alembic import op
 import sqlalchemy as sa
 
-
 # revision identifiers, used by Alembic.
 revision = '4a332edaaf7c'
 down_revision = '29bc6f1ac83a'
@@ -17,8 +16,11 @@ depends_on = None
 
 
 def upgrade():
+    op.add_column('users',
+                  sa.Column('phone_number', sa.String(), nullable=True, unique=True))
     pass
 
 
 def downgrade():
+    op.drop_column('users', 'phone_number')
     pass
